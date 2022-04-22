11203af564feca129f1f1d75794a4ecea69c806f
TShelby1 <94877568+TShelby1@users.noreply.github.com>
2022-04-22 15:06:15 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navBar';
+import Footer from './components/footer';
+import Catalog from './components/catalog';
+import Todo from './components/todo';
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import About from './components/about';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+        
+        <Catalog></Catalog>
+        <About></About>
+        <Todo></Todo>
+        <Footer></Footer>
+    </div>
+     
+      
+  );
+}
+
+export default App;
---
+++ src/components/todo.jsx
@@ -0,0 +1,22 @@
+import "./todo.css";
+
+const Todo = () => {
+    return(
+        <div className="todo">
+            <h3>Shopping List</h3>
+
+            <div className="form">
+                <input type="text"></input>
+                <button className="btn btn-sm btn-primary">Add</button>
+
+            </div>
+
+            <div className="list">
+                
+            </div>
+        </div>
+        
+    );
+};
+
+export default Todo;
